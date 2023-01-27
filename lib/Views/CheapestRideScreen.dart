ff26946eed0d0211b0a79c292f2f31612f617887
Amira <ameerh.alharbi@hotmail.com>
2023-01-28 01:02:12 +0300

    testttttttingggg

--- lib/Views/CheapestRideScreen.dart
+++ lib/Views/CheapestRideScreen.dart
@@ -1,12 +1,65 @@
-import 'package:flutter/material.dart';
 
-class CheapestRideScreen  extends StatelessWidget {
-  const CheapestRideScreen ({Key? key}) : super(key: key);
+
+import 'package:flutter/material.dart';
+import '../Controllers/ResultController.dart';
+
+class CheapestRideScreen  extends StatefulWidget {
+
+
+  CheapestRideState createState() => CheapestRideState();
+}
+
+class CheapestRideState extends State<CheapestRideScreen>
+{
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("This is CheapestRideScreen"),
+
+    ResultControllerState().getUberPriceEstimates() ;
+    ResultControllerState().getUberTimeEstimates();
+    ResultControllerState().getBoltPriceEstimates();
+    ResultControllerState().getBoltTimeEstimates();
+    print("UBER display---------");
+    ResultControllerState().joinList("Uber", ResultControllerState().uberPriceList, ResultControllerState().uberTimeList);
+    print("BOLT display---------");
+    ResultControllerState().joinList("Bolt", ResultControllerState().boltPriceList, ResultControllerState().boltTimeList);
+    print("PRICE display---------");
+    ResultControllerState.priceCompare();
+
+    return Scaffold(
+      //Check if there is available rides or not
+      body: ResultControllerState.rideResultList.length > 0
+          ? ListView.separated(
+        itemCount: ResultControllerState.rideResultList.length,
+        itemBuilder: (context, int index) {
+          num time = ResultControllerState.rideResultList[index].estimate_time/60;
+          return ListTile(
+            //if app name == Bolt present bolt img
+            leading:  ResultControllerState.rideResultList[index].app_name.toString() == "Bolt" ?
+            const CircleAvatar(
+              radius: 23,
+              backgroundImage: AssetImage("assets/Bolt_Logo.png",),
+            )
+            //if app name == Uber present uber img
+                : const CircleAvatar(
+              radius: 23,
+              backgroundImage: AssetImage("assets/Uber_Logo.png",),
+            ),
+            title: Text(ResultControllerState.rideResultList[index].display_name),
+            subtitle: Text("${time.toInt()} min to arrive"),
+            trailing: Text("${ResultControllerState.rideResultList[index].estimate_price} SAR"),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) =>
+        const Divider(
+          color: Colors.white,
+        ),
+      )
+          : const Center(child: Text("Sorry,There is no available rides right now :("),),
+
     );
   }
 }
--- lib/Views/FastestRideScreen.dart
+++ lib/Views/FastestRideScreen.dart
@@ -1,12 +1,61 @@
-import 'package:flutter/material.dart';
 
-class FastestRideScreen  extends StatelessWidget {
-  const FastestRideScreen ({Key? key}) : super(key: key);
+
+import 'package:flutter/material.dart';
+import '../Controllers/ResultController.dart';
+
+class FastestRideScreen  extends StatefulWidget
+{
+  FastestRideScreenState createState()=> FastestRideScreenState();
+}
+class FastestRideScreenState extends State<FastestRideScreen>{
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("This is FastestRideScreen :)))"),
+    ResultControllerState().getUberPriceEstimates() ;
+    ResultControllerState().getUberTimeEstimates();
+    ResultControllerState().getBoltPriceEstimates();
+    ResultControllerState().getBoltTimeEstimates();
+    print("UBER display---------");
+    ResultControllerState().joinList("Uber", ResultControllerState().uberPriceList, ResultControllerState().uberTimeList);
+    print("BOLT display---------");
+    ResultControllerState().joinList("Bolt", ResultControllerState().boltPriceList, ResultControllerState().boltTimeList);
+    print("PRICE display---------");
+    ResultControllerState.timeCompare();
+
+    return Scaffold(
+      body: ResultControllerState.rideResultList.length > 0
+          ? ListView.separated(
+        itemCount: ResultControllerState.rideResultList.length,
+        itemBuilder: (context, int index) {
+          num time = ResultControllerState.rideResultList[index].estimate_time/60;
+          return ListTile(
+            //if app name == Bolt present bolt img
+            leading:  ResultControllerState.rideResultList[index].app_name.toString() == "Bolt" ?
+            const CircleAvatar(
+              radius: 23,
+              backgroundImage: AssetImage("assets/Bolt_Logo.png",),
+            )
+            //if app name == Uber present uber img
+                : const CircleAvatar(
+              radius: 23,
+              backgroundImage: AssetImage("assets/Uber_Logo.png",),
+            ),
+            title: Text(ResultControllerState.rideResultList[index].display_name),
+            subtitle: Text("${time.toInt()} min to arrive"),
+            trailing: Text("${ResultControllerState.rideResultList[index].estimate_price} SAR"),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) =>
+        const Divider(
+          color: Colors.white,
+        ),
+      )
+          : const Center(child: Text("Sorry,There is no available rides right now :("),),
+
+
     );
   }
 }
+
