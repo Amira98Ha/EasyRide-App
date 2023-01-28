218a36963981f15d958782b537b07bb202ad0459
Amira <ameerh.alharbi@hotmail.com>
2023-01-28 19:02:08 +0300

    Edit

--- lib/Views/CheapestRideScreen.dart
+++ lib/Views/CheapestRideScreen.dart
@@ -1,4 +1,10 @@
-
+import '../Models/UberAPI/Uber.dart';
+import '../Models/UberAPI/UberPrices.dart';
+import '../Models/UberAPI/UberTimes.dart';
+import '../Models/BoltAPI/Bolt.dart';
+import '../Models/BoltAPI/BoltPrices.dart';
+import '../Models/BoltAPI/BoltTimes.dart';
+import '../Models/RideResult.dart';
 
 import 'package:flutter/material.dart';
 import '../Controllers/ResultController.dart';
@@ -11,6 +17,113 @@ class CheapestRideScreen  extends StatefulWidget {
 
 class CheapestRideState extends State<CheapestRideScreen>
 {
+  var start_latitude = 21.580948130893006;
+  var start_longitude = 39.1806807119387;
+  var end_latitude = 21.627725155960892;
+  var end_longitude = 39.11108797417971;
+
+  // list to display result
+  List<RideResult> rideResultList = [];
+
+  // for uber app
+  Uber uberOpject = new Uber();
+  List<UberPrices> uberPriceList = [];
+  List<UberTimes> uberTimeList = [];
+
+  // for bolt app
+  Bolt boltOpject = new Bolt();
+  List<BoltPrices> boltPriceList = [];
+  List<BoltTimes> boltTimeList = [];
+
+
+  Future<void> searchRides() async {
+    await getUberPriceEstimates();
+    await getUberTimeEstimates();
+    await getBoltPriceEstimates();
+    await getBoltTimeEstimates();
+    print("UBER display---------");
+    joinList("Uber", uberPriceList, uberTimeList);
+    print("BOLT display---------");
+    joinList("Bolt", boltPriceList, boltTimeList);
+    print("PRICE display---------");
+    priceCompare();
+  }
+
+  // Price Estimates for uber app
+  Future<void> getUberPriceEstimates() async {
+    uberPriceList = await uberOpject.uberPriceEstimates
+        .getPrice(start_latitude, start_longitude, end_latitude, end_longitude);
+  }
+
+  // Time Estimates for uber app
+  Future<void> getUberTimeEstimates() async {
+    uberTimeList = await uberOpject.uberTimeEstimates
+        .getTime(start_latitude, start_longitude);
+  }
+
+  // Price Estimates for bolt app
+  Future<void> getBoltPriceEstimates() async {
+    boltPriceList = await boltOpject.boltPriceEstimates
+        .getPrice(start_latitude, start_longitude, end_latitude, end_longitude);
+  }
+
+  // Time Estimates for bolt app
+  Future<void> getBoltTimeEstimates() async {
+    boltTimeList = await boltOpject.boltTimeEstimates
+        .getTime(start_latitude, start_longitude);
+  }
+
+  // join price list and time list
+  void joinList(var app_name, List priceList, List timeList) {
+    for (var i = 0; i < priceList.length; i++) {
+      for (var j = 0; j < timeList.length; j++) {
+        // check if product id identical
+        if (priceList[i].product_id == timeList[j].product_id) {
+          // create new rideResult object
+          RideResult rideResultObject = RideResult(
+              app_name,
+              priceList[i].product_id,
+              priceList[i].display_name,
+              priceList[i].estimate,
+              timeList[j].estimate,
+              priceList[i].low_estimate,
+              priceList[i].high_estimate);
+
+          // add objet to rideResultList
+          rideResultList.add(rideResultObject);
+
+          // remove object from uberTimeList
+          timeList.removeAt(j);
+
+          // exit timeList
+          break;
+        }
+      } // end for
+    } // end for
+  }
+
+  //Sort list in term of cheapest
+  void priceCompare() async {
+    //sort price ascending
+    rideResultList.sort((a, b) => a.estimate_price.compareTo(b.estimate_price));
+
+    //choose first index for cheapest
+    // var cheapRide= rideResultList[0].low_estimate;
+    // for(var i = 0; i < rideResultList.length; i++){
+    //   if(rideResultList[i].low_estimate < cheapRide) {
+    //     //store the cheapest ride using its product it
+    //     cheapRide= rideResultList[i].product_id;
+    //     rideResultList.add(cheapRide);
+    //   }
+    //   else if(rideResultList[i].low_estimate == cheapRide){
+    //     //check later
+    //
+    //   }
+    // }
+
+  }
+
+
 
 
 
@@ -18,27 +131,29 @@ class CheapestRideState extends State<CheapestRideScreen>
   @override
   Widget build(BuildContext context) {
 
-    ResultControllerState().getUberPriceEstimates() ;
-    ResultControllerState().getUberTimeEstimates();
-    ResultControllerState().getBoltPriceEstimates();
-    ResultControllerState().getBoltTimeEstimates();
-    print("UBER display---------");
-    ResultControllerState().joinList("Uber", ResultControllerState().uberPriceList, ResultControllerState().uberTimeList);
-    print("BOLT display---------");
-    ResultControllerState().joinList("Bolt", ResultControllerState().boltPriceList, ResultControllerState().boltTimeList);
-    print("PRICE display---------");
-    ResultControllerState.priceCompare();
+    return FutureBuilder<void>(
+        future: searchRides(), // function to search for rides
+    builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+    // if searchRides() has not finish
+    if (snapshot.connectionState == ConnectionState.waiting) {
+    return Center(child: Text('Please wait its loading...'));
+    }
+    else {
+    // if searchRides() has error
+    if (snapshot.hasError)
+    return Center(child: Text('Error: ${snapshot.error}'));
+    else {
 
     return Scaffold(
       //Check if there is available rides or not
-      body: ResultControllerState.rideResultList.length > 0
-          ? ListView.separated(
-        itemCount: ResultControllerState.rideResultList.length,
+      body: rideResultList.isNotEmpty?
+           ListView.separated(
+        itemCount: rideResultList.length,
         itemBuilder: (context, int index) {
-          num time = ResultControllerState.rideResultList[index].estimate_time/60;
+          num time = rideResultList[index].estimate_time/60;
           return ListTile(
             //if app name == Bolt present bolt img
-            leading:  ResultControllerState.rideResultList[index].app_name.toString() == "Bolt" ?
+            leading:  rideResultList[index].app_name.toString() == "Bolt" ?
             const CircleAvatar(
               radius: 23,
               backgroundImage: AssetImage("assets/Bolt_Logo.png",),
@@ -48,9 +163,9 @@ class CheapestRideState extends State<CheapestRideScreen>
               radius: 23,
               backgroundImage: AssetImage("assets/Uber_Logo.png",),
             ),
-            title: Text(ResultControllerState.rideResultList[index].display_name),
+            title: Text(rideResultList[index].display_name),
             subtitle: Text("${time.toInt()} min to arrive"),
-            trailing: Text("${ResultControllerState.rideResultList[index].estimate_price} SAR"),
+            trailing: Text("${rideResultList[index].estimate_price} SAR"),
           );
         },
         separatorBuilder: (BuildContext context, int index) =>
@@ -59,7 +174,11 @@ class CheapestRideState extends State<CheapestRideScreen>
         ),
       )
           : const Center(child: Text("Sorry,There is no available rides right now :("),),
-
     );
-  }
-}
+    } // else
+    } // else
+    },
+    );
+  } // build
+
+} // ResultScreen
--- lib/Views/FastestRideScreen.dart
+++ lib/Views/FastestRideScreen.dart
@@ -1,5 +1,11 @@
 
-
+import '../Models/UberAPI/Uber.dart';
+import '../Models/UberAPI/UberPrices.dart';
+import '../Models/UberAPI/UberTimes.dart';
+import '../Models/BoltAPI/Bolt.dart';
+import '../Models/BoltAPI/BoltPrices.dart';
+import '../Models/BoltAPI/BoltTimes.dart';
+import '../Models/RideResult.dart';
 import 'package:flutter/material.dart';
 import '../Controllers/ResultController.dart';
 
@@ -8,31 +14,132 @@ class FastestRideScreen  extends StatefulWidget
   FastestRideScreenState createState()=> FastestRideScreenState();
 }
 class FastestRideScreenState extends State<FastestRideScreen>{
+  var start_latitude = 21.580948130893006;
+  var start_longitude = 39.1806807119387;
+  var end_latitude = 21.627725155960892;
+  var end_longitude = 39.11108797417971;
+
+  // list to display result
+  List<RideResult> rideResultList = [];
+
+  // for uber app
+  Uber uberOpject = new Uber();
+  List<UberPrices> uberPriceList = [];
+  List<UberTimes> uberTimeList = [];
+
+  // for bolt app
+  Bolt boltOpject = new Bolt();
+  List<BoltPrices> boltPriceList = [];
+  List<BoltTimes> boltTimeList = [];
+
+
+  Future<void> searchRides() async {
+    await getUberPriceEstimates();
+    await getUberTimeEstimates();
+    await getBoltPriceEstimates();
+    await getBoltTimeEstimates();
+    print("UBER display---------");
+    joinList("Uber", uberPriceList, uberTimeList);
+    print("BOLT display---------");
+    joinList("Bolt", boltPriceList, boltTimeList);
+    print("PRICE display---------");
+    timeCompare();
+  }
+
+  // Price Estimates for uber app
+  Future<void> getUberPriceEstimates() async {
+    uberPriceList = await uberOpject.uberPriceEstimates
+        .getPrice(start_latitude, start_longitude, end_latitude, end_longitude);
+  }
+
+  // Time Estimates for uber app
+  Future<void> getUberTimeEstimates() async {
+    uberTimeList = await uberOpject.uberTimeEstimates
+        .getTime(start_latitude, start_longitude);
+  }
+
+  // Price Estimates for bolt app
+  Future<void> getBoltPriceEstimates() async {
+    boltPriceList = await boltOpject.boltPriceEstimates
+        .getPrice(start_latitude, start_longitude, end_latitude, end_longitude);
+  }
+
+  // Time Estimates for bolt app
+  Future<void> getBoltTimeEstimates() async {
+    boltTimeList = await boltOpject.boltTimeEstimates
+        .getTime(start_latitude, start_longitude);
+  }
+
+  // join price list and time list
+  void joinList(var app_name, List priceList, List timeList) {
+    for (var i = 0; i < priceList.length; i++) {
+      for (var j = 0; j < timeList.length; j++) {
+        // check if product id identical
+        if (priceList[i].product_id == timeList[j].product_id) {
+          // create new rideResult object
+          RideResult rideResultObject = RideResult(
+              app_name,
+              priceList[i].product_id,
+              priceList[i].display_name,
+              priceList[i].estimate,
+              timeList[j].estimate,
+              priceList[i].low_estimate,
+              priceList[i].high_estimate);
+
+          // add objet to rideResultList
+          rideResultList.add(rideResultObject);
+
+          // remove object from uberTimeList
+          timeList.removeAt(j);
+
+          // exit timeList
+          break;
+        }
+      } // end for
+    } // end for
+  }
+
+  //Sort list in term of cheapest
+
+
+  void timeCompare() {
+    //sort time descending
+    rideResultList.sort((a, b) => a.estimate_time.compareTo(b.estimate_time));
+    /*int fastestRide = rideResultList[0].estimate_time;
+    for (var i = 0; i < rideResultList.length; i++) {
+      if (rideResultList[i].estimate_time > fastestRide) {
+        fastestRide = rideResultList[i].product_id;
+      }
+    }*/
+  }
+
 
 
 
   @override
   Widget build(BuildContext context) {
-    ResultControllerState().getUberPriceEstimates() ;
-    ResultControllerState().getUberTimeEstimates();
-    ResultControllerState().getBoltPriceEstimates();
-    ResultControllerState().getBoltTimeEstimates();
-    print("UBER display---------");
-    ResultControllerState().joinList("Uber", ResultControllerState().uberPriceList, ResultControllerState().uberTimeList);
-    print("BOLT display---------");
-    ResultControllerState().joinList("Bolt", ResultControllerState().boltPriceList, ResultControllerState().boltTimeList);
-    print("PRICE display---------");
-    ResultControllerState.timeCompare();
+    return FutureBuilder<void>(
+        future: searchRides(), // function to search for rides
+    builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+    // if searchRides() has not finish
+    if (snapshot.connectionState == ConnectionState.waiting) {
+    return Center(child: Text('Please wait its loading...'));
+    }
+    else {
+    // if searchRides() has error
+    if (snapshot.hasError)
+    return Center(child: Text('Error: ${snapshot.error}'));
+    else {
 
     return Scaffold(
-      body: ResultControllerState.rideResultList.length > 0
-          ? ListView.separated(
-        itemCount: ResultControllerState.rideResultList.length,
+      body: rideResultList.isNotEmpty?
+           ListView.separated(
+        itemCount: rideResultList.length,
         itemBuilder: (context, int index) {
-          num time = ResultControllerState.rideResultList[index].estimate_time/60;
+          num time = rideResultList[index].estimate_time/60;
           return ListTile(
             //if app name == Bolt present bolt img
-            leading:  ResultControllerState.rideResultList[index].app_name.toString() == "Bolt" ?
+            leading:  rideResultList[index].app_name.toString() == "Bolt" ?
             const CircleAvatar(
               radius: 23,
               backgroundImage: AssetImage("assets/Bolt_Logo.png",),
@@ -42,9 +149,9 @@ class FastestRideScreenState extends State<FastestRideScreen>{
               radius: 23,
               backgroundImage: AssetImage("assets/Uber_Logo.png",),
             ),
-            title: Text(ResultControllerState.rideResultList[index].display_name),
+            title: Text(rideResultList[index].display_name),
             subtitle: Text("${time.toInt()} min to arrive"),
-            trailing: Text("${ResultControllerState.rideResultList[index].estimate_price} SAR"),
+            trailing: Text("${rideResultList[index].estimate_price} SAR"),
           );
         },
         separatorBuilder: (BuildContext context, int index) =>
@@ -54,8 +161,12 @@ class FastestRideScreenState extends State<FastestRideScreen>{
       )
           : const Center(child: Text("Sorry,There is no available rides right now :("),),
 
-
     );
-  }
-}
+    } // else
+    } // else
+    },
+    );
+  } // build
+
+} // ResultScreen
 
